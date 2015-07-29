a78ce3f8e5472778113131e4c3d6b55f3394087e
IVAN <IVAN>
2015-07-29 12:28:03 +0300

    add ShopController

---
+++ LightPointTest/Models/ViewModels/ShopViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LightPointTest.Models.ViewModels
+{
+    public class ShopViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
