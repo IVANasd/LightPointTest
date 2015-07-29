381c69187d87661c3520c62b0dc7499b67655187
IVAN <IVAN>
2015-07-29 16:00:26 +0300

    change ignoreRoute (add "")

--- LightPointTest/App_Start/RouteConfig.cs
+++ LightPointTest/App_Start/RouteConfig.cs
@@ -11,7 +11,8 @@ namespace LightPointTest
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}"); 
+            routes.IgnoreRoute(""); 
 
             routes.MapRoute(
                 name: "Default",
