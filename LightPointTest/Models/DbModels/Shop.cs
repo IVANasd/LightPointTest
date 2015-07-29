e1abc05a54e18c91b56ebc4f29cb6c5498ff65f4
IVAN <IVAN>
2015-07-29 12:10:05 +0300

    Add Models

---
+++ LightPointTest/Models/DbModels/Shop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LightPointTest.Models.DbModels
+{
+    public class Shop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        //TODO WorkTime
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ LightPointTest/Models/LptDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using LightPointTest.Models.DbModels;
+
+namespace LightPointTest.Models
+{
+    public class LptDbContext:DbContext
+    {
+        public DbSet<Shop> Shops { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public LptDbContext()
+            : base("LptDB")
+        {
+
+        }
+    }
+}
