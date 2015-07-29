5b7de1f075aa731699a3961ddaffccc2f0e79f46
IVAN <IVAN>
2015-07-29 13:03:06 +0300

    add ShopWebApiController

---
+++ LightPointTest/Controllers/ShopWebApiController.cs
@@ -0,0 +1,43 @@
+﻿using LightPointTest.Models;
+using LightPointTest.Models.DbModels;
+using LightPointTest.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LightPointTest.Controllers
+{
+    [RoutePrefix("Shop")]
+    public class ShopWebApiController : ApiController
+    {
+        private LptDbContext db = new LptDbContext();
+        
+        
+        [Route("")]
+        public IEnumerable<ShopViewModel> GetAll()
+        {
+            var shops = db.Shops.Select(s => new ShopViewModel()
+            {
+                Id = s.Id,
+                Name = s.Name,
+                Address = s.Address
+            });
+            return shops;
+        }
+
+        [Route("{shopId}/Products")]
+        public IEnumerable<ProductViewModel> GetProducts(int shopId)
+        {
+            var products = db.Shops.Find(shopId).Products.Select(
+                p=>new ProductViewModel(){
+                    Id = p.Id,
+                    Name = p.Name,
+                    Description = p.Description
+            });
+            return products;
+        }
+    }
+}
